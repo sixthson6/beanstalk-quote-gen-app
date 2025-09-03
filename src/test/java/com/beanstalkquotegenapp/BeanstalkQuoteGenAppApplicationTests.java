bad765b3719d041cac23d9c0e554accc73fd325b
sixthson6 <sudais2408@gmail.com>
2025-09-03 10:40:47 +0000

    feat: Initial project setup with html

---
+++ src/test/java/com/beanstalkquotegenapp/BeanstalkQuoteGenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanstalkquotegenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstalkQuoteGenAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
